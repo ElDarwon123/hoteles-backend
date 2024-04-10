690d71d420e6fd8973615064ed307a211217f1f7
ElDarwon123 <dsgc0614@gmail.com>
2024-04-10 01:26:17 -0500

    cors settings

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Autozitation']
+})
   const port = process.env.PORT || 4000;
   await app.listen(port);
 }
